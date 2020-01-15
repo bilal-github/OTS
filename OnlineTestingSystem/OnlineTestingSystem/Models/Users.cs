ba842948bc98e27500d3cab01ef66628cb66f4b7
bilal-github <57675296+bilal-github@users.noreply.github.com>
2020-01-15 10:18:25 -0700

    Updated bootstrap and AddUsers.cshtml is validated

--- OnlineTestingSystem/OnlineTestingSystem/Models/Users.cs
+++ OnlineTestingSystem/OnlineTestingSystem/Models/Users.cs
@@ -7,14 +7,23 @@ using System.Web;
 namespace OnlineTestingSystem.Models
 {
     public class Users
-    {    
+    {
         public int UserID { get; set; }
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage ="First Name is Required")]
+        public string UserFirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is Required")]
+        public string UserLastName { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$",
+        ErrorMessage = "Please enter correct email address")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage ="Password is Required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string UserPassword { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public string UserEmail { get; set; }
+       
     }
 }
