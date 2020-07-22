6dec8eacccbdf5f00cae98c53d6d06e2201bc5ed
MunirRizwan <rizwanmtci@gmail.com>
2020-07-21 22:39:12 -0700

    Update Admin.cs
    Here I add 2 properties and 1 AddAdmin Method

--- OnlineTestingSystem/OnlineTestingSystem/Models/Admin.cs
+++ OnlineTestingSystem/OnlineTestingSystem/Models/Admin.cs
@@ -14,13 +14,21 @@ namespace OnlineTestingSystem.Models
         [Required(ErrorMessage = "Admin Name is Required")]
         public string AdminName { get; set; }
 	    public	int AdminPhone;
+		public String Admin_Hobby;
+		public int Admin_Salary;
 
 
         [Required(ErrorMessage = "Password is Required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string AdminPassword { get; set; }
-
+        
+		
+		//Start Admin Method
+		void AddAdmin(){
+			
+			
+		}
 
 
     }
